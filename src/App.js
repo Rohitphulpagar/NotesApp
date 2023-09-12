996bf4cc6a6ed60e3f17882645da23dd2679d94b
unknown <rohit.phulpagar98@gmail.com>
2023-09-12 13:41:23 +0530

    document formated successfully

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-
 import { screen } from "@testing-library/react";
 import Mobile from "./Component/Moblie view/Mobile";
-import Desktop from './DeskTop View/Desktop'
+import Desktop from "./DeskTop View/Desktop";
 import UseSizeProperty from "./UseSizeProperty";
 function App() {
-  const{width,height}=UseSizeProperty();
+  const { width, height } = UseSizeProperty();
   return (
     <div className="App">
-  
+      {/* {window.innerWidth > 700 ?(<Mobile/>):(<Desktop/>)} */}
 
-   {/* {window.innerWidth > 700 ?(<Mobile/>):(<Desktop/>)} */}
-
-{width>560?<Desktop/>:<Mobile/>}
+      {width > 560 ? <Desktop /> : <Mobile />}
     </div>
   );
 }
--- src/DeskTop View/Desktop.js
+++ src/DeskTop View/Desktop.js
@@ -10,11 +10,13 @@ function Desktop(props) {
   const [visible, setVisible] = useState(false);
   const [inputText, setInputText] = useState(" ");
   const [inputDescription, setInputDescription] = useState(" ");
- 
+
   const [show, setShow] = useState(true);
   const [groupSelected, setGroupSelected] = useState(null);
- 
-  const [color1, setColor] = useState(["#B38BFA,#FF79F2,#43E6FC,#F19576,#0047FF,#6691FF"]);
+
+  const [color1, setColor] = useState([
+    "#B38BFA,#FF79F2,#43E6FC,#F19576,#0047FF,#6691FF",
+  ]);
 
   const submitButton = (e) => {
     e.preventDefault();
@@ -62,7 +64,6 @@ function Desktop(props) {
     setColor(["#6691FF"]);
   }
 
-
   const ClickOnAction = () => {
     setShow(!show);
   };
@@ -85,22 +86,21 @@ function Desktop(props) {
     event.preventDefault();
   };
   const currentDates = format(new Date(), "  hh:mm a ,  do MMMM yyyy ");
-  const loadData=localStorage.getItem("groups")
-  ? JSON.parse(localStorage.getItem("groups"))
-  :[];
-  
+  const loadData = localStorage.getItem("groups")
+    ? JSON.parse(localStorage.getItem("groups"))
+    : [];
+
   const [groupNames, setGroupNames] = useState(loadData);
- 
+
   useEffect(() => {
     window.localStorage.setItem("groups", JSON.stringify(groupNames));
-  },[groupNames]);
-
+  }, [groupNames]);
 
   return (
     <div className="StartJourney">
-    {/* <div className="desktopApp"> */}
-     
-     <div className="firstHalf">
+      {/* <div className="desktopApp"> */}
+
+      <div className="firstHalf">
         <section>
           <div className="AppHeadings">
             <h1 className="topicName">Pocket Notes</h1>
@@ -120,7 +120,8 @@ function Desktop(props) {
               <label className="groupN">
                 Group Name{" "}
                 <input
-                  type="text" className="inputTags"
+                  type="text"
+                  className="inputTags"
                   value={inputText}
                   placeholder="Enter your group name.."
                   onChange={handleGroup}
@@ -128,61 +129,70 @@ function Desktop(props) {
               </label>
               <div className="colorPicker">
                 <div className="colorDivs">
-                <span className="chooseColors">Choose colour&nbsp;&nbsp;&nbsp;</span>
-                <span className="colors">
-                <input
-                  type="radio"
-                  onClick={handleColor1}
-                  name="pick-color1"
-                  value="#B38BFA"
-                  className="FirstColors" checked
-                />
-                <input
-                  type="radio"
-                  onClick={handleColor2}
-                  name="pick-color2"
-                  value="#FF79F2"
-                  className="SecondColors" checked
-                />
-                <input
-                  type="radio"
-                  onClick={handleColor3}
-                  name="pick-color3"
-                  value="#43E6FC"
-                  className="ThirdColors" checked
-                />
-                <input
-                  type="radio"
-                  onClick={handleColor4}
-                  name="pick-color4"
-                  value="#F19576"
-                  className="FourthColors" checked
-                />
-                <input
-                  type="radio"
-                  onClick={handleColor5}
-                  name="pick-color5"
-                  value="#0047FF"
-                  className="FifthColors" checked
-                />
-                <input
-                  type="radio"
-                  onClick={handleColor6}
-                  name="pick-color6"
-                  value="#6691FF"
-                  className="SixthColors" checked
-                />
-                </span></div>
+                  <span className="chooseColors">
+                    Choose colour&nbsp;&nbsp;&nbsp;
+                  </span>
+                  <span className="colors">
+                    <input
+                      type="radio"
+                      onClick={handleColor1}
+                      name="pick-color1"
+                      value="#B38BFA"
+                      className="FirstColors"
+                      checked
+                    />
+                    <input
+                      type="radio"
+                      onClick={handleColor2}
+                      name="pick-color2"
+                      value="#FF79F2"
+                      className="SecondColors"
+                      checked
+                    />
+                    <input
+                      type="radio"
+                      onClick={handleColor3}
+                      name="pick-color3"
+                      value="#43E6FC"
+                      className="ThirdColors"
+                      checked
+                    />
+                    <input
+                      type="radio"
+                      onClick={handleColor4}
+                      name="pick-color4"
+                      value="#F19576"
+                      className="FourthColors"
+                      checked
+                    />
+                    <input
+                      type="radio"
+                      onClick={handleColor5}
+                      name="pick-color5"
+                      value="#0047FF"
+                      className="FifthColors"
+                      checked
+                    />
+                    <input
+                      type="radio"
+                      onClick={handleColor6}
+                      name="pick-color6"
+                      value="#6691FF"
+                      className="SixthColors"
+                      checked
+                    />
+                  </span>
+                </div>
               </div>
-             <div className="createss">
-             <button
-                className="creat"
-                onChange={() => setVisible(false)}
-                onClick={submitButton}
-              >
-                Create
-              </button>
-              </div> 
+              <div className="createss">
+                <button
+                  className="creat"
+                  onChange={() => setVisible(false)}
+                  onClick={submitButton}
+                >
+                  Create
+                </button>
+              </div>
             </ReactModal>
           </p>{" "}
           <div className="groupNamess">
@@ -196,77 +206,76 @@ function Desktop(props) {
                 key={index}
               >
                 <span
-                  style={{ background:color1 }}
+                  style={{ background: color1 }}
                   className="profileNames"
                   key={index}
                 >
                   {group.name.slice(0, 2)}
-                </span><span className="GroupNamesCategorys">
-                {group.name}</span>
+                </span>
+                <span className="GroupNamesCategorys">{group.name}</span>
               </li>
             ))}
           </div>
         </section>
-</div>
+      </div>
 
       {!show && (
-<div className="divSection2"> 
-        <section className="sections2">
-          <div className="sector11">
-            {groupSelected && (
-              <div>
-                <div className="headingSection22">
-                <span className="headingSection33">
-            <button className="btnArrs"
-              onClick={() => {
-                setShow(true);
-              }}
-            >
-             <span className="images"> <img src={LOGO} alt="logo" /></span>
-            </button>
-            
-
-                 <span className="profileNames2">{groupSelected.name.slice(0,2)}</span>
-                  <span className="profiles">{groupSelected.name}</span>
-           
-                </span></div>
+        <div className="divSection2">
+          <section className="sections2">
+            <div className="sector11">
+              {groupSelected && (
                 <div>
-                  <form onSubmit={SubmitDescription}>
-                      <div className="combineTwos">
-                      <div className="date">
-                        {currentDates}
-                        </div>
-                    <div className="descs">
-                      {groupSelected.description}
-                    </div>
-
-                      </div>
-                    <div className="textAreass">
-                      <textarea 
-                        value={groupSelected.description}
-                        onChange={(e) => WriteGroupDescription(e.target.value)}
-                        placeholder="Enter your text here..."
-                        onKeyUp={(e) => {
-                          if (e.key === "Enter" && !e.shiftKey) {
-                            e.preventDefault();
-                            e.target.value = " ";
-                          }
+                  <div className="headingSection22">
+                    <span className="headingSection33">
+                      <button
+                        className="btnArrs"
+                        onClick={() => {
+                          setShow(true);
                         }}
-                      ></textarea>
-                    </div>
-                  </form>{" "}
+                      >
+                        <span className="images">
+                          {" "}
+                          <img src={LOGO} alt="logo" />
+                        </span>
+                      </button>
+
+                      <span className="profileNames2">
+                        {groupSelected.name.slice(0, 2)}
+                      </span>
+                      <span className="profiles">{groupSelected.name}</span>
+                    </span>
+                  </div>
+                  <div>
+                    <form onSubmit={SubmitDescription}>
+                      <div className="combineTwos">
+                        <div className="date">{currentDates}</div>
+                        <div className="descs">{groupSelected.description}</div>
+                      </div>
+                      <div className="textAreass">
+                        <textarea
+                          value={groupSelected.description}
+                          onChange={(e) =>
+                            WriteGroupDescription(e.target.value)
+                          }
+                          placeholder="Enter your text here..."
+                          onKeyUp={(e) => {
+                            if (e.key === "Enter" && !e.shiftKey) {
+                              e.preventDefault();
+                              e.target.value = " ";
+                            }
+                          }}
+                        ></textarea>
+                      </div>
+                    </form>{" "}
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        </section></div>
+              )}
+            </div>
+          </section>
+        </div>
       )}
-      </div>
-   
-  )}
+    </div>
+  );
+}
 
-
-
-
-
-  export default Desktop;
+export default Desktop;
